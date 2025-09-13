b5bab1afd91357edd03e9bb469d94d3ee364c332
spigell <Spigelly@gmail.com>
2025-09-14 01:41:26 +0700

    remove unused cloud import in hcloud test program (#21)

--- integration-tests/testdata/programs/hcloud-go/main.go
+++ integration-tests/testdata/programs/hcloud-go/main.go
@@ -4,52 +4,20 @@ import (
 	"fmt"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"github.com/spigell/pulumi-talos-cluster/integration-tests/pkg/cloud"
 	hcloud "github.com/spigell/pulumi-talos-cluster/integration-tests/pkg/cloud/hcloud"
 	"github.com/spigell/pulumi-talos-cluster/integration-tests/pkg/cluster"
 	talospkg "github.com/spigell/pulumi-talos-cluster/integration-tests/pkg/talos"
 )
 
-var (
-	platform   = "hcloud"
-	talosImage = "ghcr.io/siderolabs/installer:v1.11.0"
-)
-
-var clu = &cluster.Cluster{
-	PrivateNetwork:    "10.10.10.0/24",
-	PrivateSubnetwork: "10.10.10.0/25",
-	KubernetesVersion: "1.32.0",
-	Machines: []*cluster.Machine{
-		{
-			ID:         "controlplane-1",
-			Type:       "init",
-			Platform:   platform,
-			TalosImage: talosImage,
-			ServerType: "cx22",
-			PrivateIP:  "10.10.10.5",
-			Datacenter: "fsn1-dc14",
-		},
-		{
-			ID:         "worker-1",
-			Type:       "worker",
-			Platform:   platform,
-			TalosImage: talosImage,
-			ServerType: "cx22",
-			PrivateIP:  "10.10.10.3",
-			Datacenter: "fsn1-dc14",
-		},
-	},
-}
-
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+		clu, err := cluster.Load("cluster.yaml")
+		if err != nil {
+			return err
+		}
 		clu.Name = ctx.Stack()
 
-		var (
-			provider cloud.Provider
-			err      error
-		)
-		provider, err = hcloud.NewWithIPS(ctx, clu)
+		provider, err := hcloud.NewWithIPS(ctx, clu)
 		if err != nil {
 			return err
 		}
